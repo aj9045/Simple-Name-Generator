418209aac77fc14b1a9437c4af5369869c208b8a
aj9045 <identity_unveiled@hotmail.com>
2016-02-08 21:40:42 -0500

    add conditional to show that there are no saved names or not

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -106,7 +106,7 @@ class GeneratorButtonBox extends React.Component {
 
 class SavedList extends React.Component {
   render() {
-    var names = this.props.savedNames.map(function(name, index){
+    var names = this.props.savedNames.length === 0 ? <p>No saved names yet!</p> : this.props.savedNames.map(function(name, index){
       return (
         <SavedName key={index} name={name} index={index} onDelete={this.props.onDelete}/>
       );
