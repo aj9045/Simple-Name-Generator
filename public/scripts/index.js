620cfb53e8510b4a66a1df516af6b16e0d2d7c2c
aj9045 <identity_unveiled@hotmail.com>
2016-02-08 20:12:30 -0500

    add constructor with two empty states and simple render

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,25 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstArray: [],
+      lastArray: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Name Generator</h1>
+        <GeneratorBox />
+        <SavedList  />
+      </div>
+    );
+  }
+}
+
+
+
 ReactDOM.render(
   <App />,
   document.getElementById("content")
